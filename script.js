59350e31b612c9119204c5935efbfd030705587a
Ananthakrishnan Balajee <86157960+ananthakrishna7@users.noreply.github.com>
2023-06-18 15:06:58 +0000

    Added placeholder files
    Must work on these to build our project.

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-// document.write("HELLO");
-// window.alert("baba");
-// console.log("heh");
-// document.getElementById("demo").innerHTML = "HELLO";
+// var firstsec = document.getElementsByClassName('intro');
+// var secondsec = document.getElementsByClassName('header_text');
+// var lastScrollTop = 0;
+// window.onscroll = function(){
+//    var st = window.scrollY || document.documentElement.scrollTop; 
+//    if (st > lastScrollTop){
+//       secondsec.scrollIntoView({behavior: "smooth"});
+//    } else {
+//       firstsec.scrollIntoView({behavior: "smooth"});
+//    }
+//    lastScrollTop = st <= 0 ? 0 : st; 
+// } 
